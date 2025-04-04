fda2596d374917098d844745ce57581e06b7ef0b
brandencall <brandencall@live.com>
2025-04-03 20:01:23 -0600

    Removing remap for pasting system clipboard

--- lua/brabs/remap.lua
+++ lua/brabs/remap.lua
@@ -12,7 +12,6 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set({ "n", "v" }, "<leader>pc", "\"+p")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+y")
